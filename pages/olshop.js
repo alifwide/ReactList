5ec88ba0cb9d42cc2d997386e67ce86221eef0db
Alipwide <alifwide@gmail.com>
2021-02-13 19:55:04 +0700

    penambahan olshop

--- pages/olshop.js
+++ pages/olshop.js
@@ -1,129 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+import React from 'react'
+import Card from "../components/Cardolshop"
 import $ from "jquery"
-
-
-
-
-class List extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: [
-				{ eventName: "Hari Peringatan Laut dan Samudera nasional - 15 Januari", editable: false }
-				, { eventName: "Hari Lahan Basah Sedunia - 2 Februari", editable: false }
-				, { eventName: "Hari Peduli Sampah Nasional - 21 Februari[1]", editable: false }
-				, { eventName: "Hari Hutan Sedunia - 21 Maret", editable: false }
-				, { eventName: "Hari Air Sedunia - 22 Maret", editable: false }
-				, { eventName: "Hari Meteorologi Sedunia - 23 Maret", editable: false }
-				, { eventName: "Hari Bumi - 22 April", editable: false }
-				, { eventName: "Hari Penanaman Pohon - Jumat terakhir di bulan April", editable: false }
-				, { eventName: "Hari Burung Migratori Internasional - 3 Mei", editable: false }
-				, { eventName: "Hari Surya - 3 Mei", editable: false }
-				, { eventName: "Hari Biodiversitas Dunia - 22 Mei", editable: false }
-				, { eventName: "Hari Bersepeda Ke Kantor (Bike-to-Work Day) - Jumat Ketiga di bulan Mei", editable: false }
-				, { eventName: "Hari Anti Tembakau Internasional - 31 Mei", editable: false }
-				, { eventName: "Hari Lingkungan Hidup Sedunia PBB - 5 Juni", editable: false }
-				, { eventName: "Hari Melawan Desertifikasi dan Kekeringan Dunia PBB - 17 Juni", editable: false }
-				, { eventName: "Hari Populasi Dunia PBB - 11 Juli", editable: false }
-				, { eventName: "Hari Perlindungan Lapisan Ozon Sedunia - 16 September", editable: false }
-				, { eventName: "Hari Emisi Nol (Zero Emissions Day) - 20 September", editable: false }
-				, { eventName: "Hari Bebas Mobil (Car Free Day) - 22 September", editable: false }
-				, { eventName: "eDay - 4 Oktober di 2008", editable: false }
-				, { eventName: "Hari Habitat Dunia PBB - Senin pertama di bulan Oktober", editable: false }
-				, { eventName: "Hari Pengurangan Bencana Alam Internasional - 13 Oktober", editable: false }
-				, { eventName: "Hari Peringatan Sedunia untuk Mencegah Eksploitasi Lingkungan dalam Perang dan Konflik Bersenjata - 6 November", editable: false }
-				, { eventName: "Hari Pohon - 21 November", editable: false }
-				, { eventName: "Hari Gunung Sedunia - 11 Desember", editable: false }
-				, { eventName: "Hari Aksi Ozon - Pada waktu tertentu di musim panas", editable: false }
-			]
-		}
-	}
-
-	render = () => {
-		return (
-			<table className="table">
-				<thead>
-					<th>Nama-Tanggal</th>
-				</thead>
-				<tbody>
-					{this.state.data.map((data, index) => (
-						<SubTable
-							index={index}
-							eventName={data.eventName}
-							key={index}
-						/>
-					))}
-				</tbody>
-			</table>
-		)
-	}
-}
-
-class SubTable extends List {
-	render = () => {
-		const makeEditable = () => {
-			let data = this.state.data;
-			data[this.props.index].editable = !data[this.props.index].editable;
-			this.setState({data:data});
-		}
-		return (
-			<tr key={this.props.index}>
-				<td contentEditable={this.state.data[this.props.index].editable} >{this.props.eventName}</td>
-				<td><button className="btn btn-primary" onClick={makeEditable} >Edit</button></td>
-			</tr>
-		)
-	}
-}
-
-class Card extends React.Component{
-    render(){
-        return(
-            <div className="col-lg-6 col-sm-12 p-2">
-                <div className="card">
-                    <div className="card-body row">
-                        {/*menampilkan  gambar/cover */}
-                        <div className="col-5">
-                            <img src={this.props.cover} className="img" height="200"/>
-                        </div>
-
-                        {/** menampilkan deskripsi */}
-                        <div className="col-7">
-                            <h5 className="text-info">
-                                {this.props.judul}
-                            </h5>
-                            <h6 className="text-dark">
-                               Penulis: {this.props.penulis}
-                            </h6>
-                            <h6 className="text-dark">
-                               Penerbit: {this.props.penerbit}
-                            </h6>
-                            <h6 className="text-danger">
-                               Harga: Rp {this.props.harga}
-                            </h6>
-
-                            {/** button untuk mengedit */}
-                            <button className="btn btn-sm btn-primary m-1" onClick={this.props.onEdit}>
-                                Edit
-                            </button>
-
-                            {/** button untuk menghapus */}
-                            <button className="btn btn-sm btn-danger m-1" onClick={this.props.onDrop}>
-                                Hapus
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+import "bootstrap/dist/js/bootstrap.bundle"
 
 //menyambungkan
-class Olshop extends React.Component {
-    constructor(props){
-        super(props)
+class olshop extends React.Component {
+    constructor(){
+        super()
         this.state = {
             buku: [
                 {
@@ -258,7 +141,6 @@ class Olshop extends React.Component {
             <input type="text" className="form-control my-2" placeholder="pencarian" value={this.state.keyword} onChange={ev => this.setState({keyword: ev.target.value})} onKeyUp={ev => this.searching(ev)}/>
                <div className="row">
                     {this.state.filterBuku.map( (item, index) => (
-						console.log(item),
                         <Card 
                             judul={ item.judul}
                             penulis={ item.penulis}
@@ -314,51 +196,5 @@ class Olshop extends React.Component {
     }
 }
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <nav class="navbar navbar-expand-lg navbar-light bg-primary">
-                <div className="container">
-                    <a class="navbar-brand text-white click1" href="#">Tugas</a>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item click">
-                                <Link to="/list" className="nav-link text-white">list</Link>
-                            </li>
-                            <li class="nav-item click">
-                                <Link to="/olshop" className="nav-link text-white">shop</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        )
-    }
-}
-
-
-class App extends React.Component{
-	constructor(props){
-		super(props);
-	}
-	render = () => {
-		return(
-			<BrowserRouter>
-				<Navbar/>
-
-				<Switch>
-					<Route path="/olshop">
-						<Olshop/>
-					</Route>
-					<Route path="/list">
-						<List/>
-					</Route>
-				</Switch>
-			</BrowserRouter>
-		)
-	}
-}
-
-ReactDOM.render(<App />, document.getElementById("root"))
-
-
+//export
+export default olshop;
